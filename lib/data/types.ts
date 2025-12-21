5194901706fb2ea19d8df851402014301bd21b52
Lily <haquynhchi@gmail.com>
2025-12-21 21:39:38 +1100

    Added Food interface and mock food data

---
+++ lib/data/types.ts
@@ -0,0 +1,28 @@
+export interface Food {
+  food_id: string;
+  food_name: string;
+  food_type: "Brand" | "Generic";
+  brand_name?: string;
+  serving_unit: "g" | "ml";
+  serving_size: number;
+  preferred_serving_size: number;
+  calories: number;
+  carbohydrate: number;
+  protein: number;
+  fat: number;
+  saturated_fat?: number;
+  polyunsaturated_fat?: number;
+  monounsaturated_fat?: number;
+  cholesterol?: number;
+  sodium?: number;
+  potassium?: number;
+  fiber?: number;
+  sugar?: number;
+  vitamin_a?: number;
+  vitamin_c?: number;
+  calcium?: number;
+  iron?: number;
+  trans_fat?: number;
+  added_sugars?: number;
+  vitamin_d?: number;
+}
